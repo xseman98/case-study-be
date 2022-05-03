89c25801dc5d676bfc4d196206960ed2d7d3d43e
xseman98 <78860937+xseman98@users.noreply.github.com>
2022-05-03 09:11:02 +0200

    added CORS policy

--- main.py
+++ main.py
@@ -1,34 +1,50 @@
 from fastapi import FastAPI
 import requests
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+api_key = "8af0ed745d-545d45f398-rb7wfs"
 
 @app.get("/currencies")
 async def currencies():
-    url = "https://api.fastforex.io/currencies?api_key=8af0ed745d-545d45f398-rb7wfs"
+    url = "https://api.fastforex.io/currencies?api_key={0}".format(api_key)
     headers = {"Accept": "application/json"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        return response.json()['currencies']
+        result = []
+        for k, v in response.json()['currencies'].items():
+            result.append({'code': k, 'name': v})
+        return result
     return {}
 
 @app.get("/latest")
 async def latest(base: str | None = None):
     if base:
-        url = "https://api.fastforex.io/fetch-all?from={0}&api_key=8af0ed745d-545d45f398-rb7wfs".format(base)
+        url = "https://api.fastforex.io/fetch-all?from={0}&api_key={1}".format(base, api_key)
     else:
-        url = "https://api.fastforex.io/fetch-all?api_key=8af0ed745d-545d45f398-rb7wfs"
+        url = "https://api.fastforex.io/fetch-all?api_key={0}".format(api_key)
     headers = {"Accept": "application/json"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        return response.json()['results']
+        result = []
+        for k, v in response.json()['results'].items():
+            result.append({'code': k, 'value': v})
+        return result
     return {}
 
 @app.get("/convert")
 async def convert(amount : float, toCurrency: str, fromCurrency : str | None = None):
     if fromCurrency:
-        url = "https://api.fastforex.io/convert?from={0}&to={1}&amount={2}&api_key=8af0ed745d-545d45f398-rb7wfs".format(fromCurrency, toCurrency, amount)
+        url = "https://api.fastforex.io/convert?from={0}&to={1}&amount={2}&api_key={3}".format(fromCurrency, toCurrency, amount, api_key)
     else:
-        url = "https://api.fastforex.io/convert?to={1}&amount={2}&api_key=8af0ed745d-545d45f398-rb7wfs".format(toCurrency, amount)
+        url = "https://api.fastforex.io/convert?to={0}&amount={1}&api_key={2}".format(toCurrency, amount, api_key)
     headers = {"Accept": "application/json"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
